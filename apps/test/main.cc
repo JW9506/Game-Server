a38cd09cbdcbc6afac2a39f1c02a1d733bf5904c
JW9506 <cf.dota@gmail.com>
2021-03-17 07:05:06 -0400

    comment out logger demo

--- apps/test/main.cc
+++ apps/test/main.cc
@@ -2,10 +2,22 @@
 #include "netbus.h"
 #include "proto_man.h"
 #include "pf_cmd_map.h"
+// #include "logger.h"
+// #include "time_list.h"
+
+// void cb(void*) {
+//     static int i = -1;
+//     log_debug("hello %d", ++i);
+//     log_warning("HELLO %d", i);
+// }
 
 int main(int argc, char** argv) {
     proto_man::init(PROTO_BUF);
     init_pf_cmd_map();
+
+    // logger::init("logger/netbus", "netbus_log");
+    // schedule(cb, NULL, 3000, -1);
+
     netbus::instance()->start_tcp_server(6080);
     netbus::instance()->start_tcp_server(6081);
     netbus::instance()->start_ws_server(8001);
