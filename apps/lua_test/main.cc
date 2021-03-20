c9e5fe76f85f83201786e15ce86c7714f3cc6ed4
JW9506 <cf.dota@gmail.com>
2021-03-19 21:56:30 -0400

    require static tolua lib

--- apps/lua_test/main.cc
+++ apps/lua_test/main.cc
@@ -7,16 +7,16 @@
 
 int main(int argc, char** argv) {
     proto_man::init(PROTO_BUF);
+    lua_wrapper::init();
     init_pf_cmd_map();
     logger::init("logger/netbus", "netbus_log", 1);
-
-    lua_wrapper::init();
-    lua_wrapper::execute_file("./scripts/main.lua");
 
     netbus::instance()->tcp_server(6080);
     netbus::instance()->tcp_server(6081);
     netbus::instance()->ws_server(8001);
     netbus::instance()->udp_server(8002);
+
+    lua_wrapper::execute_file("./scripts/main.lua");
 
     netbus::instance()->run();
     lua_wrapper::exit();
