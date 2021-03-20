5ea6ed7f208acfbdb0a93c6db70e95618ad7d112
JW9506 <cf.dota@gmail.com>
2021-03-19 22:39:41 -0400

    move some start up code to lua

--- apps/lua_test/main.cc
+++ apps/lua_test/main.cc
@@ -6,15 +6,8 @@
 #include "lua_wrapper.h"
 
 int main(int argc, char** argv) {
-    proto_man::init(PROTO_BUF);
     lua_wrapper::init();
     init_pf_cmd_map();
-    logger::init("logger/netbus", "netbus_log", 1);
-
-    netbus::instance()->tcp_server(6080);
-    netbus::instance()->tcp_server(6081);
-    netbus::instance()->ws_server(8001);
-    netbus::instance()->udp_server(8002);
 
     lua_wrapper::execute_file("./scripts/main.lua");
 
--- apps/lua_test/scripts/main.lua
+++ apps/lua_test/scripts/main.lua
@@ -1,1 +1,15 @@
-print("foo")
+
+logger.init("logger/netbus", "netbus_log", 1);
+
+local proto_type = {
+  PROTO_JSON = 0,
+  PROTO_BUF = 1,
+}
+proto_man.init(proto_type.PROTO_BUF)
+
+netbus.tcp_server(6080)
+netbus.tcp_server(6081)
+netbus.ws_server(8001)
+netbus.udp_server(8002)
+
+print("Lua starting up!")
